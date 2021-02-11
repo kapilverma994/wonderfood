e5b2f20034a5d3b9ee64d3439c88971193f8b0ae
kapilverma994 <74894827+kapilverma994@users.noreply.github.com>
2021-02-11 10:36:55 +0530

    wishlist start

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
+use DB;
 class WishlistController extends Controller
 {
     public function addwishlist($id){
-     
+    $user_id=Auth::user()->id;
+    $check=DB::table('wishlists')->where('user_id',$user_id)->where('product_id',$id)->first();
+   $data=array(
+       'user_id'=>$user_id,
+       'product_id'=>$id,
+       'created_at'=>now(),
+       'updated_at'=>now(),
+   );
+   if(Auth::check()){
+if($check){
+    Toastr()->warning('Product already in your wishlist');
+    return redirect()->back();
+
+}else{
+    DB::table('wishlists')->insert($data);
+    Toastr()->success('Product successfully added in your wishlist');
+    return redirect()->back();
+}
+
+   }else{
+    Toastr()->danger('Please login first ');
+    return redirect()->back();
+
+   }
+   
     }
 }
